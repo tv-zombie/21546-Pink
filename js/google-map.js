233ff570d28760d67b8159a9db91eda31001db8d
Kate <po4ta500@mail.ru>
2015-04-06 04:23:36 +0300

    все декоративные элементы добавлены

---
+++ js/google-map.js
@@ -0,0 +1,16 @@
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(59.9373,30.3214)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+  var image = "../img/map-point.png";
+  var myLatLng = new google.maps.LatLng(59.9361947,30.3218419,17);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
