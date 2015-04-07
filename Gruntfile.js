dfb20370467c24f2bf2bcdf0f116414501428553
Kate <po4ta500@mail.ru>
2015-04-07 07:06:30 +0300

    настроен gruntfile, пережаты картинки

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,21 @@
 module.exports = function(grunt) {
 
+ require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+
+    // watch
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
     // Lint Spaces in code
     lintspaces: {
       all: {
@@ -23,10 +36,63 @@ module.exports = function(grunt) {
           showCodes: true
         }
       }
+      },
+      // Compile sass
+      sass: {
+        dist: {
+          options: {
+            style: 'expanded'
+          },
+          files: {
+            'css/style.css' : ["scss/style.scss"]
+          }
+        }
+      },
+      // autoprefix
+      autoprefixer: {
+        options: {
+          browsers: ["last 4 version"]
+        },
+        style: {
+          src: "css/style.css"
+        }
+      },
+      //combine media queris
+      cmq: {
+        style: {
+          files: {
+            "css/style.css":["css/style.css"]
+          }
+        }
+      },
+      //cssmin
+      cssmin: {
+        style: {
+          files: {
+            "css.min.css":["css/style.css"]
+          }
+        }
+      },
+      // image min
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src:["img/*.{png,jpg,gif,svg}"]
+          }]
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-lintspaces');
-
-  grunt.registerTask('lint', ['lintspaces']);
+  grunt.registerTask('default', [
+    'watch',
+    "sass",
+    "autoprefixer",
+    "cmq",
+    "cssmin",
+    "imagemin"
+    ]);
 };
